3607765eee46ffd4da11dc792ce3705051ffea10
siyabonga majola <Siyabonga.Majola@hollywoodbets.net>
2022-07-13 17:03:50 +0200

    new

---
+++ Test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string senderID = "EPSON";
+string termID = "00001";
+string message = termID + ":::" + senderID + "+" + "";
+
+var dispay = message.Split('+');
+Console.WriteLine(dispay[0]);

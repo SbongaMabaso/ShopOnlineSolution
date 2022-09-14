1652ea0abb66ca4bb87e88b1943b22f44716d6d7
Sbonga Mabaso <mabasos@wavesoft.co.za>
2022-09-14 16:34:08 +0200

    Improving code peformance

--- ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -25,15 +25,14 @@ namespace ShopOnline.Api.Controllers
             {
                 //can call the stop proccedure here.
                 var products = await this.productRepository.GetItems();
-                var productCategories = await this.productRepository.GetCategories();
 
-                if(products == null || productCategories == null)
+                if(products == null)
                 {
                     return NotFound();
                 }
                 else
                 {
-                    var productDtos = products.ConvertToDto(productCategories);
+                    var productDtos = products.ConvertToDto();
 
                     return Ok(productDtos);
 
@@ -60,8 +59,7 @@ namespace ShopOnline.Api.Controllers
                 }
                 else
                 {
-                    var productCategory = await this.productRepository.GetCategory(product.CategoryId);
-                    var productDto = product.ConvertToDto(productCategory);
+                    var productDto = product.ConvertToDto();
                     return Ok(productDto);
                 }
             }
@@ -95,8 +93,7 @@ namespace ShopOnline.Api.Controllers
             try
             {
                 var products = await productRepository.GetItemsByCategory(categoryId);
-                var productCategories = await productRepository.GetCategories();
-                var productDtos = products.ConvertToDto(productCategories);
+                var productDtos = products.ConvertToDto();
 
                 return Ok(productDtos);
             }
--- ShopOnline.Api/Extensions/DtoConversions.cs
+++ ShopOnline.Api/Extensions/DtoConversions.cs
@@ -15,13 +15,10 @@ namespace ShopOnline.Api.Extensions
                         IconCSS = productCategory.IconCSS,
                     }).ToList();
         }
-        public static IEnumerable<ProductDto> ConvertToDto(this IEnumerable<Product> products,
-                                                            IEnumerable<ProductCategory> productCategories)
+        public static IEnumerable<ProductDto> ConvertToDto(this IEnumerable<Product> products)
         {
             //return data from data
             return (from product in products
-                    join productCategory in productCategories
-                    on product.CategoryId equals productCategory.Id
                     select new ProductDto
                     {
                         Id = product.Id,
@@ -30,13 +27,13 @@ namespace ShopOnline.Api.Extensions
                         ImageURL = product.ImageURL,
                         Price = product.Price,
                         Qty = product.Qty,
-                        CategoryId = product.CategoryId,
-                        CategoryName = productCategory.Name
+                        CategoryId = product.ProductCategory.Id,
+                        CategoryName = product.ProductCategory.Name
                     }).ToList();
         }
 
         //Filter by category
-        public static ProductDto ConvertToDto(this Product product, ProductCategory productCategory)
+        public static ProductDto ConvertToDto(this Product product)
         {
             //return data from database
             return new ProductDto
@@ -47,8 +44,8 @@ namespace ShopOnline.Api.Extensions
                 ImageURL = product.ImageURL,
                 Price = product.Price,
                 Qty = product.Qty,
-                CategoryId = product.CategoryId,
-                CategoryName = productCategory.Name
+                CategoryId = product.ProductCategory.Id,
+                CategoryName = product.ProductCategory.Name
             };
         }
 
